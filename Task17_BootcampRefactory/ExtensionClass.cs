a5455ad9c0ae6cf1be44ba0192992c4af1731181
Mia <miahljnh@gigaming.io>
2020-03-02 19:21:35 +0700

    .

---
+++ Task17_BootcampRefactory/ExtensionClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Humanizer;
+
+namespace Task17_BootcampRefactory
+{
+    public static class ExtensionClass 
+    {
+        public static string Capitalize(this string str1)
+        {
+
+            return Regex.Replace(str1, @"\b[a-z]\w+", delegate (Match match)
+            {
+                string v = match.ToString();
+                return char.ToUpper(v[0]) + v.Substring(1);
+            });
+        }
+
+        public static string SnakeCase(this string str1)
+        {
+            return Regex.Replace(str1, " ", "_");
+        }
+        
+        public static string KebabCase(this string str1)
+        {
+            return Regex.Replace(str1, " ", "-");
+        }
+
+        public static T Mode<T>(this IEnumerable<T> value)
+        {
+            return value.GroupBy(q => q).OrderByDescending(q => q.Count()).Select(k => k.Key).First();
+        }
+
+        public static string Trim(this string str1, int number)
+        {
+            return $"{str1.Substring(0, number)}. . .";
+        }
+
+        public static string TrimWords(this string str1, int number)
+        {
+            return String.Join(" ", str1.Split(' ').Take(number));
+        }
+
+        public static string Convert(this int number)
+        {
+            string result = null;
+            List<string> satuan = new List<string>()
+            {
+                "puluh", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"
+            };
+
+            List<string> belasan = new List<string>()
+            {
+                "puluh", "se", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"
+            };
+
+            if (number < 10)
+            {
+                result = satuan[number];
+            }
+            else if(number == 10)
+            {
+                result = "sepuluh";
+            }
+            else if(number == 11)
+            {
+                result = "sebelas";
+            }
+            else if(number % 10 == 0)
+            {
+                result = belasan[number / 10] + " puluh ";
+            }
+            else if(number > 10 && number < 20)
+            {
+                result = belasan[number % 10] + " belas";
+            }
+            else if(number > 20)
+            {
+                var puluhan = number.ToString().ToCharArray().First().ToString();
+                var puluhanNumber = belasan[Int32.Parse(puluhan)] + " puluh ";
+                var satuanNumber = satuan[number % 10];
+                result = puluhanNumber + satuanNumber;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task17_BootcampRefactory/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task17_BootcampRefactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan kata yang anda inginkan;");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            //Number 1
+            var hello1 = str1.Capitalize();
+            Console.WriteLine($"Capitalize : {hello1}");
+            Console.WriteLine("\n");
+
+            //Number2
+            var hello2 = str1.SnakeCase();
+            Console.WriteLine($"SnakeCase : {hello2}");
+            Console.WriteLine("\n");
+
+            //Number 3
+            var hello3 = str1.KebabCase();
+            Console.WriteLine($"KebabCase : {hello3}");
+            Console.WriteLine("\n");
+
+            //Number 4
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 6, 8, 8, 6, 9 };
+            var grades = new[] { 87.5, 88.5, 60.5, 90.5, 88.5 };
+            var mostNumbers = numbers.Mode();
+            var mostGrades = grades.Mode();
+            Console.WriteLine($"Mode of Numbers : {mostNumbers}");
+            Console.WriteLine($"Mode of Grades : {mostGrades}");
+            Console.WriteLine("\n");
+
+            //Number 5
+            Console.WriteLine("Masukkan angka yang anda inginkan;");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var result = num.Convert();
+            Console.WriteLine($"Convert Integer To String : {result}");
+            Console.WriteLine("\n");
+
+            //Number 6
+            Console.WriteLine("Masukkan jumlah teks yang anda ingin pisahkan;");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            var tulisanPanjang1 = str1.Trim(num1);
+            Console.WriteLine($"Trim Text : {tulisanPanjang1}");
+            Console.WriteLine("\n");
+
+            //Number 7
+            Console.WriteLine("Masukkan jumlah teks yang anda ingin pisahkan;");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            var tulisanPanjang2 = str1.TrimWords(num2);
+            Console.WriteLine($"Trim Words : {tulisanPanjang2}");
+            Console.WriteLine("\n");
+        }
+    }
+
+}
